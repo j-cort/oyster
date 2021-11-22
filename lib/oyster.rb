d26a9c76defa2a6a81c39ac7a78b9cdfdcdbaee6
j-cort <81960011+j-cort@users.noreply.github.com>
2021-11-22 17:23:08 +0000

    deducts fare when touch_out

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -1,5 +1,6 @@
 class Oyster
   LIMIT = 90
+  MIN_BALANCE = 1
   attr_reader :balance
   attr_reader :in_use
 
@@ -12,21 +13,31 @@ class Oyster
     @balance + amount > LIMIT
   end
 
+  def enough_balance?
+    @balance >= MIN_BALANCE
+  end
+
   def top_up(amount)
-    raise "This top up would exceed your £#{Oyster::LIMIT} card limit." if exceed_limit?(amount)
+    raise "This top up would exceed your £#{LIMIT} card limit." if exceed_limit?(amount)
     @balance += amount
   end
   
+  private 
+
   def deduct(fare)
     @balance -= fare
   end
+  
+  public
 
   def touch_in
+    raise "You have less than the £#{MIN_BALANCE} minimum balance, please top up." unless enough_balance?
     @in_use = true
   end
 
   def touch_out
     @in_use = false
+    deduct(MIN_BALANCE)
   end
 
   def in_journey?
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -19,30 +19,45 @@ describe Oyster do
     expect{ card.top_up(Oyster::LIMIT + 1) }.to raise_error "This top up would exceed your £#{Oyster::LIMIT} card limit."
   end
 
-  it "reduce the balance by a specified amount" do
-    card.top_up(5)
-    expect{ card.deduct 1 }.to change{ card.balance }.by -1
-  end
+  # it "reduce the balance by a specified amount" do
+  #   card.top_up(5)
+  #   result = card.send(:deduct, 1)
+  #   expect{ result }.to change{ card.balance }.by -1
+  # end
 
   it "set in_use to be true when touch in" do
+    card.top_up(Oyster::MIN_BALANCE)
     card.touch_in
     expect(card).to be_in_journey
   end
 
   it "set in_use to be false when touch out" do
+    card.top_up(Oyster::MIN_BALANCE)
     card.touch_in
     card.touch_out
     expect(card).not_to be_in_journey
   end
 
   it "in_journey return true when touch in" do
+    card.top_up(Oyster::MIN_BALANCE)
     card.touch_in
     expect(card.in_journey?).to eq true
   end
 
   it "in_journey return false when touch_in and touch_out" do
+    card.top_up(Oyster::MIN_BALANCE)
     card.touch_in
     card.touch_out
     expect(card.in_journey?).to eq false
+  end
+
+  it "does not allow user to touch in if card is under the minimum balance" do
+    expect { card.touch_in }.to raise_error "You have less than the £#{Oyster::MIN_BALANCE} minimum balance, please top up."
+  end
+
+  it "deducts the mimimum fare after touch_out" do
+    card.top_up(Oyster::MIN_BALANCE)
+    card.touch_in
+    expect { card.touch_out }.to change{card.balance}.by(-Oyster::MIN_BALANCE)
   end
 end
